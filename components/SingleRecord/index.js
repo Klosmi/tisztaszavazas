7f5e6423b71cb64053d3737a70a8ef63261fc92f
Gergő Gazda <info@gazdagergo.com>
2020-10-06 09:32:04 +0200

    add map to szk

--- components/SingleRecord/index.js
+++ components/SingleRecord/index.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react';
+
 import {
+  Collapse,
   Spin,
+  Descriptions,
+  Tabs,
+  Table,
+  Tag,
+  Typography,
+  Space,
 } from 'antd';
-import { ObjectInspector, TableInspector } from 'react-inspector';
+import { ObjectInspector } from 'react-inspector';
 import tszService from '../../services/tszService';
 import styled from 'styled-components';
 import { AppContext } from '../../pages';
+import SzkMap from '../SzkMap'
+
+const { Panel } = Collapse;
+const { Item } = Descriptions;
+const { TabPane } = Tabs;
+const { Text } = Typography;
 
 const InspectorStyle = styled.div`
   ol > li {
@@ -20,6 +34,17 @@ const InspectorStyle = styled.div`
     }
   }
 `
+
+const columns = [
+  {
+    title: 'Közterület (rész)',
+    dataIndex: 'leiras',
+  },
+  {
+    title: 'Oldal',
+    dataIndex: 'megjegyzes',
+  },
+]
 
 export default ({ id }) => {
   const [singleSzkResult, setSingleSzkResult] = useState()
@@ -37,12 +62,72 @@ export default ({ id }) => {
 
   if (!singleSzkResult) return <Spin />
 
+  const {
+    szavazokorCime,
+    szavazokorSzama,
+    kozteruletek,
+    kozigEgyseg: {
+      kozigEgysegNeve,
+      kozigEgysegSzavazokoreinekSzama,
+      megyeNeve,
+    },
+    valasztokerulet: {
+      leiras: valasztokeruletLeirasa
+    },
+    valasztokSzama,
+    akadalymentes,
+    szavazohelyisegHelye: {
+      coordinates: {
+        0: lng,
+        1: lat
+      }
+    },
+    korzethatar: {
+      coordinates: { 0: korzethatarCoordinates }
+    }
+  } = singleSzkResult
+
   return (
-    <InspectorStyle>
-      <ObjectInspector
-        expandLevel={Array.isArray(singleSzkResult) ? undefined : 1}
-        data={singleSzkResult}
+    <>
+  <Descriptions column={2} title={szavazokorCime}>
+  
+    <Item>{kozigEgysegNeve}</Item>
+    <Item>{valasztokeruletLeirasa}</Item>
+    <Item>{szavazokorSzama}. szavazókör</Item>
+    <Item label="Névjegyzékbe vettek száma" >{valasztokSzama}</Item>
+    <div>
+      {akadalymentes && <Tag color="green">akadálymentes</Tag>}
+      {!akadalymentes && <Tag color="red">nem akadálymentes</Tag>}
+    </div>
+    <Item label={`${megyeNeve === "Budapest" ? 'Kerület' : 'Település'} szavazóköreinek száma`} >{kozigEgysegSzavazokoreinekSzama}</Item>
+  </Descriptions>
+  <Tabs defaultActiveKey="1">
+    <TabPane tab="Közterületek" key="1">
+      <Table
+        columns={columns}
+        dataSource={kozteruletek}
       />
-    </InspectorStyle>
+    </TabPane>
+    <TabPane tab="Térkép" key="2">
+      <Space>
+        <Text type="secondary">A pontos adatok a táblázatban szerepelnek, a térképi megjelenés kizárólag tájékoztató jellegű!</Text>      
+      </Space>    
+      <SzkMap
+        center={{ lat, lng }}
+        polygonPath={korzethatarCoordinates.map(([lng, lat]) => ({ lng, lat }))}
+      />
+    </TabPane>
+  </Tabs>
+  <Collapse accordion>
+    <Panel header="Nyers adatok" key="1">
+      <InspectorStyle>
+        <ObjectInspector
+          expandLevel={Array.isArray(singleSzkResult) ? undefined : 1}
+          data={singleSzkResult}
+        />
+      </InspectorStyle>
+    </Panel>
+  </Collapse>    
+    </>
   )
 }
---
+++ components/SzkMap/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript, Marker, Polygon } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+
+const options = {
+  fillColor: "#386FB3",
+  strokeColor: "#386FB3",
+  fillOpacity: .3,
+  strokeOpacity: .8,
+  strokeWeight: 1,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  geodesic: false,
+  zIndex: 1
+}
+
+class SzkMap extends Component {
+  render() {
+    return (
+      <main>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={this.props.center}
+          zoom={15}
+        >
+          <Marker
+            position={this.props.center}
+          />
+          <Polygon
+            paths={this.props.polygonPath}
+            options={options}
+          />          
+        </GoogleMap>
+      </LoadScript>
+      </main>
+    )
+  }
+}
+
+export default SzkMap
