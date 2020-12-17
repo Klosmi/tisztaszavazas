ad7d55099c543863fd5103c7f8c69fe434e0c9e8
Gergő Gazda <info@gazdagergo.com>
2020-12-17 22:44:18 +0100

    fix api url

--- services/tszGet.js
+++ services/tszGet.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import paramSerializer from './paramSerializer'
 
 export default async ({ path, data, query, election }) => {
-  const url =`${process.env.NEXT_PUBLIC_API_BASE_URL}${path}${paramSerializer(query)}`
+  const url =`/api/tsz/${path}${paramSerializer(query)}`
 
   const { data: d, headers } =  await axios({
     url,
