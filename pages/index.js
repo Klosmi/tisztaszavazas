3c8d1d30418750703b611a7034eda8692eff3f04
Gergő Gazda <info@gazdagergo.com>
2021-01-07 18:21:40 +0100

    fix: remove route import

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import React, { useState, createContext } from 'react'
+import React from 'react'
 import Layot from '../components/Layout'
 import AllRecords from '../components/AllRecords';
-import SingleRecord from '../components/SingleRecord';
-import Menu from '../components/Menu';
-import Header from '../components/Header';
-import WhereVote from '../components/WhereVote';
-import CustomQueryContainer from '../components/CustomQueryContainer';
-import ElectionSelector from '../components/ElectionSelector';
 
 const Home = () => (
   <Layot>
