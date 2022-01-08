5f7add2c5edd0826a9ff5d87a67c5a9f1488e7ee
Gergely Gazda <info@gazdagergo.com>
2022-01-08 19:34:30 +0100

    remove unused code

--- components/AllSettlements/index.jsx
+++ components/AllSettlements/index.jsx
@@ -16,7 +16,6 @@ import useValasztas from '../../hooks/useValasztas';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import allSettlements from './settlementsSimplified.json'
 
-// const { Item } = Form
 
 const Wrap = styled.div`
   display: flex;
@@ -34,90 +33,10 @@ const PageHeaderStyled = styled(PageHeader)`
 
 const AllSettlements = ({
   election = "ogy2018",
-  // showSearch = true
 }) => {
-  // const [selectedVk, setSelectedVk] = useState()
-  // const [settlements, setSettlements] = useState([])
-  // const [settlementResult, setSettlementResult] = useState()
-
   const { leiras: electionDescription } = useValasztas({ election }) || {}
 
-  // const geoJsonToPoly = geo => (
-  //   geo?.coordinates[0].map(([lng, lat]) => ({ lng, lat }))    
-  // )
-
-  // const { getAllVks, getVkDetails } = useValasztokerulet
-
-  // const oevk = getVkDetails({ id: selectedVk, election }) || {}
-
-  // useEffect(() => {
-  //   if (!oevk.leiras) return
-  //   const query = `[
-  //     { $match: {
-  //       "valasztokerulet.leiras": "${oevk.leiras}",
-  //     } },
-  //     { $group: {
-  //         _id: "$kozigEgyseg",
-  //         szk: { $sum: 1 },
-  //         valasztokSzama: { $sum: "$valasztokSzama" }
-  //     } },  
-  //     { $sort: { "_id.kozigEgysegNeve": 1 } },
-  //     { $project: {
-  //       település: "$_id.kozigEgysegNeve",
-  //       valasztokSzama: 1,
-  //       szk: 1,
-  //       _id: 0,
-  //       rank: 1      
-  //     } }
-  //   ]`
-  //   tszService.aggregate({ query, election })
-  //   .then(({ data }) => setSettlements(data))
-  //   .catch(e => console.log(e))
-  // },[oevk, election])
-
-  // const allVks = getAllVks({ election })
-
-  // const [lng, lat] = oevk?.korzethatar?.coordinates[0][0] || [19, 47]
-
-  // useEffect(() => {
-  //   if (!settlements?.length) {
-  //     setSettlementResult(null)
-  //     return
-  //   }
-
-  //   const s = settlements.map(({ település }) => település.replace('.ker', '. kerület'))
-
-  //   const query = `[
-  //     { $match: {
-  //         name: { $in: ${JSON.stringify(s)}  }
-  //     }}
-  //   ]`
-
-  //   zipService.aggregate({ query, path: '/settlements' })
-  //   .then(({ data }) => setSettlementResult(data))
-
-  // }, [settlements])
-
   const { lg } = useBreakpoint()
-
-  // const summary = settlements?.reduce((acc, s) => ({
-  //   szk: acc.szk + (s.szk || 0),
-  //   "választók száma": acc["választók száma"] + s.valasztokSzama,
-  //   település: acc.település + 1,
-  // }),{
-  //   szk: 0,
-  //   "választók száma": 0,
-  //   település: 0,
-  // })
-
-  // const viewData = [
-  //   ...settlements.map(({ szk,valasztokSzama, település }) => ({
-  //     szk,
-  //     "választók száma": valasztokSzama,
-  //     település
-  //   })),
-  //   summary
-  // ]
 
   return (
     <>
@@ -128,46 +47,26 @@ const AllSettlements = ({
           breadcrumbName: electionDescription || '...',
         }]}}
       />
-      {/* {showSearch && (
-        <Item
-        >
-          <Select
-            showSearch
-            placeholder="Választókerület keresése"
-            onSelect={setSelectedVk}
-            options={allVks?.map(({ _id: value, leiras: label }) => ({ value, label }))}
-            filterOption={optionFilter}
-            notFoundContent={<div>Betöltés...</div>}
-          />
-        </Item>
-      )} */}
       <Wrap horizontal={lg}>
-        {/* {oevk && ( */}
-          <MapWrap>
-            <MapBase
-              center={{ lat: 47, lng: 19 }}
-              zoom={7.5}
-              mapId="85b71dbefa7b82fa"
-            >
-              {/* <MapBase.EvkPolygon
-                unfilled
-                paths={geoJsonToPoly(oevk?.korzethatar)}
-              />  */}
-              {allSettlements.features?.map?.(({ geometry }) => (
-                <>
-                  <MapBase.SzkPolygon
-                    geometry={geometry}
-                  />
-                </>
-              ))}
-            </MapBase>
-            {/* <Legend stroke="#FF3333AA" fill="#386FB300" text="OEVK határ" /> */}
-            <Legend stroke="#386FB3CC" fill="#386FB355" text="Település-határok" />
-          </MapWrap>
-        {/* )} */}
-        {/* {viewData && (
-          <ReactJson json={viewData} />
-        )}       */}
+        <MapWrap>
+          <MapBase
+            center={{ lat: 47, lng: 19 }}
+            zoom={7.5}
+            mapId="85b71dbefa7b82fa"
+          >
+            {allSettlements.features?.map?.(({ geometry, settlementType }) => (
+              <MapBase.SzkPolygon
+                geometry={geometry}
+                options={settlementType === 'capital' ? {
+                  fillColor: "transparent",
+                  strokeWeight: 3,
+                } : {}}
+              />
+            ))}
+          </MapBase>
+          {/* <Legend stroke="#FF3333AA" fill="#386FB300" text="OEVK határ" /> */}
+          <Legend stroke="#386FB3CC" fill="#386FB355" text="Település-határok" />
+        </MapWrap>
       </Wrap>
     </>
   )
